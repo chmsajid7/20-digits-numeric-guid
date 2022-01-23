ff07045dcec80e323d56572de9a1a8aabfe74248
hp <hp@DESKTOP-NC0MAAO>
2022-01-23 06:02:51 +0500

    Add project files.

---
+++ numeric guid/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace numeric_guid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = Guid.NewGuid().ToByteArray();
+            var bufferToNumeric = BitConverter.ToInt64(buffer, 0);
+            var value = bufferToNumeric.ToString();
+            var str = "0" + value.ToString();
+
+            Console.WriteLine($"{str}\n{str.Length}");
+        }
+    }
+}
